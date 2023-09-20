77928f14b21023f5aa505ebb15f0bed4486622e5
abbastoof <abbas.toof@gmail.com>
2023-09-20 17:22:11 +0300

    radians moving_key

--- src/utils/key_handler.c
+++ src/utils/key_handler.c
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 12:04:42 by vvu               #+#    #+#             */
-/*   Updated: 2023/09/20 16:22:39 by atoof            ###   ########.fr       */
+/*   Updated: 2023/09/20 17:20:40 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	mouse_handler(t_cub3d *data)
 static void	move(t_cub3d *data, int height, int width)
 {
 	if (data->raw_map[height / (BLOCK_SIZE / PLAYER_SIZE)] \
-					[width / (BLOCK_SIZE / PLAYER_SIZE)] == '1')
+				[width / (BLOCK_SIZE / PLAYER_SIZE)] == '1')
 		return ;
 	else
 	{
@@ -33,45 +33,48 @@ static void	move(t_cub3d *data, int height, int width)
 	}
 }
 
-void moving_keys(int key, t_cub3d *data)
+void	moving_keys(int key, t_cub3d *data)
 {
-    double move_speed = 1; // Adjust the movement speed as needed
+	double	player_rad_angle;
 
-    if (key == W)
-    {
-        data->tmp_player_x -= move_speed * cos(data->player_angle * M_PI / 180);
-        data->tmp_player_y -= move_speed * sin(data->player_angle * M_PI / 180);
-    }
-    if (key == S)
-    {
-        data->tmp_player_x += move_speed * cos(data->player_angle * M_PI / 180);
-        data->tmp_player_y += move_speed * sin(data->player_angle * M_PI / 180);
-    }
-    if (key == A)
-    {
-        // Move left by subtracting 90 degrees from the player's angle
-        data->tmp_player_x += move_speed * cos((data->player_angle + 90) * M_PI / 180);
-        data->tmp_player_y += move_speed * sin((data->player_angle + 90) * M_PI / 180);
-    }
-    if (key == D)
-    {
-        // Move right by adding 90 degrees to the player's angle
-        data->tmp_player_x += move_speed * cos((data->player_angle - 90) * M_PI / 180);
-        data->tmp_player_y += move_speed * sin((data->player_angle - 90) * M_PI / 180);
-    }
-    if (key == RIGHT)
-    {
-        data->player_angle += 5;
-        if (data->player_angle >= 360)
-            data->player_angle -= 360;
-    }
-    if (key == LEFT)
-    {
-        data->player_angle -= 5;
-        if (data->player_angle < 0)
-            data->player_angle += 360;
-    }
-    printf("Angle: %f\n", data->player_angle);
+	player_rad_angle = data->player_angle * M_PI / 180.0;
+	if (key == W)
+	{
+		data->tmp_player_x -= cos(player_rad_angle);
+		data->tmp_player_y -= sin(player_rad_angle);
+	}
+	if (key == S)
+	{
+		data->tmp_player_x += cos(player_rad_angle);
+		data->tmp_player_y += sin(player_rad_angle);
+	}
+	if (key == A)
+	{
+		data->tmp_player_x -= sin(player_rad_angle);
+		data->tmp_player_y += cos(player_rad_angle);
+	}
+	if (key == D)
+	{
+		data->tmp_player_x += sin(player_rad_angle);
+		data->tmp_player_y -= cos(player_rad_angle);
+	}
+	if (key == RIGHT)
+	{
+		data->player_angle += 90;
+		if (data->player_angle >= 360)
+			data->player_angle -= 360;
+	}
+	if (key == LEFT)
+	{
+		data->player_angle -= 90;
+		if (data->player_angle < 0)
+			data->player_angle += 360;
+	}
+	if (data->player_angle >= 360)
+		data->player_angle -= 360;
+	if (data->player_angle < 0)
+		data->player_angle += 360;
+	printf("Angle: %f\n", data->player_angle);
 }
 
 int	key_handler(int key, t_cub3d *data)
@@ -84,11 +87,11 @@ int	key_handler(int key, t_cub3d *data)
 		free_texture(data);
 		exit(0);
 	}
-	if (key == W || key == A || key == S || key == D || key == LEFT || \
+	if (key == W || key == A || key == S || key == D || key == LEFT ||
 		key == RIGHT)
 	{
 		moving_keys(key, data);
-		if (data->player_x != data->tmp_player_x || \
+		if (data->player_x != data->tmp_player_x ||
 			data->player_y != data->tmp_player_y)
 			move(data, data->tmp_player_y, data->tmp_player_x);
 	}
