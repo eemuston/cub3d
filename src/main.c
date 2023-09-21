8071bd5678aa1ccd8cd631b292694c7f1c6f79ad
eemuston <eemuston@student.hive.fi>
2023-09-21 13:17:48 +0300

    Uncommenting some of the fucntions

--- src/main.c
+++ src/main.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vvu <vvu@student.hive.fi>                  +#+  +:+       +#+        */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 15:07:50 by eemuston          #+#    #+#             */
-/*   Updated: 2023/09/20 10:56:00 by vvu              ###   ########.fr       */
+/*   Updated: 2023/09/20 12:49:06 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/cub3d.h"
 
-// void	hook_keys_loop(t_cub3d *data)
-// {
-// 	mlx_hook(data->mlx_window, 2, 1L << 1, key_handler, data);
-// 	mlx_hook(data->mlx_window, 17, 0, mouse_handler, data);
-// 	mlx_loop(data->mlx_ptr);
-// }
+void	hook_keys_loop(t_cub3d *data)
+{
+	mlx_hook(data->mlx_window, 2, 1L << 1, key_handler, data);
+	mlx_hook(data->mlx_window, 17, 0, mouse_handler, data);
+	mlx_loop(data->mlx_ptr);
+}
 
 int	main(int argc, char **argv)
 {
@@ -28,10 +28,9 @@ int	main(int argc, char **argv)
 		return (1);
 	if (read_file_and_parse(argv, &data) == 1)
 		return (1);
-	// init_window(&data);
-	// image_handler(&data);
-	// hook_keys_loop(&data);
-	printf("pa: %f cos: %f",data.pa, cos(data.pa));
+	init_window(&data);
+	image_handler(&data);
+	hook_keys_loop(&data);
 	free_texture(&data);
 	return (0);
 }
