0111a5d61fab38355534936604457198aed1949d
eemuston <eemuston@student.hive.fi>
2023-09-25 15:48:23 +0300

    progress

--- src/utils/key_handler.c
+++ src/utils/key_handler.c
@@ -6,7 +6,7 @@
 /*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 12:04:42 by vvu               #+#    #+#             */
-/*   Updated: 2023/09/25 12:54:48 by eemuston         ###   ########.fr       */
+/*   Updated: 2023/09/25 13:37:49 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,8 +47,8 @@ static void	move(t_cub3d *data, int height, int width)
 		return ;
 	else
 	{
-		data->player_x = width;
-		data->player_y = height;
+		data->player->player_x = width;
+		data->player->player_y = height;
 		mlx_clear_window(data->mlx_ptr, data->mlx_window);
 		render_game(data);
 	}
@@ -58,50 +58,50 @@ void	moving_keys(int key, t_cub3d *data)
 {
 	double	player_rad_angle;
 
-	player_rad_angle = data->player_angle * M_PI / 180.0;
+	player_rad_angle = data->player->player_angle * M_PI / 180.0;
 	if (key == W)
 	{
-		data->tmp_player_x -= cos(player_rad_angle);
-		data->tmp_player_y -= sin(player_rad_angle);
+		data->player->tmp_player_x -= cos(player_rad_angle);
+		data->player->tmp_player_y -= sin(player_rad_angle);
 	}
 	if (key == S)
 	{
-		data->tmp_player_x += cos(player_rad_angle);
-		data->tmp_player_y += sin(player_rad_angle);
+		data->player->tmp_player_x += cos(player_rad_angle);
+		data->player->tmp_player_y += sin(player_rad_angle);
 	}
 	if (key == A)
 	{
-		data->tmp_player_x -= sin(player_rad_angle);
-		data->tmp_player_y += cos(player_rad_angle);
+		data->player->tmp_player_x -= sin(player_rad_angle);
+		data->player->tmp_player_y += cos(player_rad_angle);
 	}
 	if (key == D)
 	{
-		data->tmp_player_x += sin(player_rad_angle);
-		data->tmp_player_y -= cos(player_rad_angle);
+		data->player->tmp_player_x += sin(player_rad_angle);
+		data->player->tmp_player_y -= cos(player_rad_angle);
 	}
 	if (key == RIGHT)
 	{
-		data->player_angle += 90;
-		if (data->player_angle >= 360)
-			data->player_angle -= 360;
+		data->player->player_angle += 90;
+		if (data->player->player_angle >= 360)
+			data->player->player_angle -= 360;
 	}
 	if (key == LEFT)
 	{
-		data->player_angle -= 90;
-		if (data->player_angle < 0)
-			data->player_angle += 360;
+		data->player->player_angle -= 90;
+		if (data->player->player_angle < 0)
+			data->player->player_angle += 360;
 	}
-	if (data->player_angle >= 360)
-		data->player_angle -= 360;
-	if (data->player_angle < 0)
-		data->player_angle += 360;
-	// printf("Angle: %f\n", data->player_angle);
+	if (data->player->player_angle >= 360)
+		data->player->player_angle -= 360;
+	if (data->player->player_angle < 0)
+		data->player->player_angle += 360;
+	// printf("Angle: %f\n", data->player->player_angle);
 }
 
 int	key_handler(int key, t_cub3d *data)
 {
-	data->tmp_player_x = data->player_x;
-	data->tmp_player_y = data->player_y;
+	data->player->tmp_player_x = data->player->player_x;
+	data->player->tmp_player_y = data->player->player_y;
 	if (key == 53)
 	{
 		mlx_destroy_window(data->mlx_ptr, data->mlx_window);
@@ -112,9 +112,9 @@ int	key_handler(int key, t_cub3d *data)
 	key == RIGHT)
 	{
 		moving_keys(key, data);
-		if (data->player_x != data->tmp_player_x || \
-			data->player_y != data->tmp_player_y)
-			move(data, data->tmp_player_y, data->tmp_player_x);
+		if (data->player->player_x != data->player->tmp_player_x || \
+			data->player->player_y != data->player->tmp_player_y)
+			move(data, data->player->tmp_player_y, data->player->tmp_player_x);
 	}
 	return (0);
 }
