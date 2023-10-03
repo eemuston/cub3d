41ef1e6319a0464c8b2a504bbce93835a86c9d12
abbastoof <abbas.toof@gmail.com>
2023-10-03 20:40:43 +0300

    return hit_point from draw_ray to be used for later

--- src/init_window/draw_fov.c
+++ src/init_window/draw_fov.c
@@ -6,28 +6,31 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 10:55:36 by mtoof             #+#    #+#             */
-/*   Updated: 2023/10/03 18:56:50 by atoof            ###   ########.fr       */
+/*   Updated: 2023/10/03 20:39:49 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../header/cub3d.h"
 
-void	draw_ray(t_cub3d *data, double angle)
+t_point	draw_ray(t_cub3d *data, double angle)
 {
 	double	distance;
 	double	ray_x;
 	double	ray_y;
+	t_point	hit_point;
+	t_point	start_pos;
 
 	distance = 0.1;
+	start_pos = (t_point){data->player->pixel_x, data->player->pixel_y};
 	ray_x = data->player->pixel_x;
 	ray_y = data->player->pixel_y;
 	while (is_in_map(data, ray_x, ray_y) && is_not_wall(data, ray_x, ray_y))
 		update_position(&ray_x, &ray_y, angle, distance);
+	hit_point.p_x = ray_x;
+	hit_point.p_y = ray_y;
 	if (is_in_map(data, ray_x, ray_y))
-	{
-		dda_algorithm((t_point){data->player->pixel_x, data->player->pixel_y}, \
-			(t_point){ray_x, ray_y}, data, (t_line){0});
-	}
+		dda_algorithm(start_pos, hit_point, data, (t_line){0});
+	return (hit_point);
 }
 
 void	draw_fov(t_cub3d *data)
