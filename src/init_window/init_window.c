4d602c1ab830decb04c0de1ee6a756b2572bfbc8
eemuston <eemuston@student.hive.fi>
2023-10-09 17:38:26 +0300

    WTFSF

--- src/init_window/init_window.c
+++ src/init_window/init_window.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_window.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: eemuston <eemuston@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 09:27:52 by vvu               #+#    #+#             */
-/*   Updated: 2023/10/09 13:36:19 by atoof            ###   ########.fr       */
+/*   Updated: 2023/10/09 17:36:20 by eemuston         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,36 +26,30 @@ int	init_window(t_cub3d *data)
 	data->color[1] = 0xFFA500;
 	data->color[2] = 0xFF0000;
 	data->mlx_ptr = mlx_init();
-	data->texture[0].img = mlx_xpm_file_to_image(data->mlx_ptr,
-			"/Users/atoof/cub3d/wall1h.xpm", &data->texture[0].width,
-			&data->texture[0].height);
-	data->texture[0].data = (int *)mlx_get_data_addr(data->texture[0].img,
-			&data->texture[0].bpp, &data->texture[0].size_line,
-			&data->texture[0].endian);
-	data->texture[1].img = mlx_xpm_file_to_image(data->mlx_ptr,
-			"texture/wall_brick_black.xpm", &data->texture[1].width,
-			&data->texture[1].height);
-	data->texture[1].data = (int *)mlx_get_data_addr(data->texture[1].img,
-			&data->texture[1].bpp, &data->texture[1].size_line,
-			&data->texture[1].endian);
-	data->texture[2].img = mlx_xpm_file_to_image(data->mlx_ptr,
-			"texture/wall_brick_gray.xpm", &data->texture[2].width,
-			&data->texture[2].height);
-	data->texture[2].data = (int *)mlx_get_data_addr(data->texture[2].img,
-			&data->texture[2].bpp, &data->texture[2].size_line,
-			&data->texture[2].endian);
-	data->texture[3].img = mlx_xpm_file_to_image(data->mlx_ptr,
-			"texture/wall_brick_orange.xpm", &data->texture[3].width,
-			&data->texture[3].height);
-	data->texture[3].data = (int *)mlx_get_data_addr(data->texture[3].img,
-			&data->texture[3].bpp, &data->texture[3].size_line,
-			&data->texture[3].endian);
-	data->brick_texture.img = mlx_xpm_file_to_image(data->mlx_ptr,
-			"/Users/atoof/cub3d/brick.xpm", &data->brick_texture.width,
-			&data->brick_texture.height);
-	data->brick_texture.data = (int *)mlx_get_data_addr(data->brick_texture.img,
-			&data->brick_texture.bpp, &data->brick_texture.size_line,
-			&data->brick_texture.endian);
+	data->texture[NORTH - 1].img = mlx_xpm_file_to_image(data->mlx_ptr,
+			data->texture[NORTH - 1].path , &data->texture[NORTH - 1].width,
+			&data->texture[NORTH - 1].height);
+	data->texture[NORTH - 1].data = (int *)mlx_get_data_addr(data->texture[NORTH - 1].img,
+			&data->texture[NORTH - 1].bpp, &data->texture[NORTH - 1].size_line,
+			&data->texture[NORTH - 1].endian);
+	data->texture[SOUTH - 1].img = mlx_xpm_file_to_image(data->mlx_ptr,
+			data->texture[SOUTH - 1].path , &data->texture[SOUTH - 1].width,
+			&data->texture[SOUTH - 1].height);
+	data->texture[SOUTH - 1].data = (int *)mlx_get_data_addr(data->texture[SOUTH - 1].img,
+			&data->texture[SOUTH - 1].bpp, &data->texture[SOUTH - 1].size_line,
+			&data->texture[SOUTH - 1].endian);
+	data->texture[WEST - 1].img = mlx_xpm_file_to_image(data->mlx_ptr,
+			data->texture[WEST - 1].path, &data->texture[WEST - 1].width,
+			&data->texture[WEST - 1].height);
+	data->texture[WEST - 1].data = (int *)mlx_get_data_addr(data->texture[WEST - 1].img,
+			&data->texture[WEST - 1].bpp, &data->texture[WEST - 1].size_line,
+			&data->texture[WEST - 1].endian);
+	data->texture[EAST - 1].img = mlx_xpm_file_to_image(data->mlx_ptr,
+			data->texture[EAST - 1].path , &data->texture[EAST - 1].width,
+			&data->texture[EAST - 1].height);
+	data->texture[EAST - 1].data = (int *)mlx_get_data_addr(data->texture[EAST - 1].img,
+			&data->texture[EAST - 1].bpp, &data->texture[EAST - 1].size_line,
+			&data->texture[EAST - 1].endian);
 	if (!data->mlx_ptr)
 		return (error_in_img(data, 1));
 	data->mlx_window = mlx_new_window(data->mlx_ptr, WIDTH, HEIGHT, "Cub3D");
