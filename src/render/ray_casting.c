9b689f19ce4431053756f69cd10cfa3289a967a9
vvu <vvu@c1r5p1.hive.fi>
2023-10-10 14:05:30 +0300

    fixed xpm file extension

--- src/render/ray_casting.c
+++ src/render/ray_casting.c
@@ -6,7 +6,7 @@
 /*   By: vvu <vvu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 10:55:36 by mtoof             #+#    #+#             */
-/*   Updated: 2023/10/09 18:48:57 by vvu              ###   ########.fr       */
+/*   Updated: 2023/10/10 14:05:00 by vvu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ static void	shortest_distance(t_cub3d *data, double ray_angle)
 static void	init_ray(t_cub3d *data, double ray_angle)
 {
 	data->ray->hit = 0;
-	data->ray->ray_pos.p_x = data->player->player_x;
-	data->ray->ray_pos.p_y = data->player->player_y;
+	data->ray->ray_pos.p_x = data->player->player_x + 0.1;
+	data->ray->ray_pos.p_y = data->player->player_y + 0.1;
 	data->ray->ray_dir.p_x = cos(ray_angle);
 	data->ray->ray_dir.p_y = -sin(ray_angle);
 	data->ray->map_x = (int)data->ray->ray_pos.p_x;
