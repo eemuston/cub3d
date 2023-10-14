994728018f6da1c49df572bba31f8b5239f34544
abbastoof <abbas.toof@gmail.com>
2023-10-14 15:04:47 +0300

    merge

--- src/bonus_src/draw_bonus.c
+++ src/bonus_src/draw_bonus.c
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 14:06:39 by atoof             #+#    #+#             */
-/*   Updated: 2023/10/13 14:06:42 by atoof            ###   ########.fr       */
+/*   Updated: 2023/10/14 14:58:56 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,13 @@ void	draw_2d_direction(t_cub3d *data)
 	line.dx = 0;
 	p1.p_x = (MINI_WIDTH / 2) + PLAYER_SIZE / 2;
 	p1.p_y = (MINI_HEIGHT / 2) + PLAYER_SIZE / 2;
-	p2.p_x = (MINI_WIDTH / 2) + PLAYER_SIZE / 2
-		+ data->player->pdx * 10;
-	p2.p_y = (MINI_HEIGHT / 2) + PLAYER_SIZE / 2
-		+ data->player->pdy * 10;
+	p2.p_x = (MINI_WIDTH / 2) + PLAYER_SIZE / 2 + data->player->pdx * 10;
+	p2.p_y = (MINI_HEIGHT / 2) + PLAYER_SIZE / 2 + data->player->pdy * 10;
 	dda_algorithm(p1, p2, data, line);
 }
 
-void	my_mlx_pixel_put_mini(t_cub3d *data, double width \
-		, double height, unsigned int color)
+void	my_mlx_pixel_put_mini(t_cub3d *data, double width, double height,
+		unsigned int color)
 {
 	char	*dst;
 	int		y;
@@ -60,8 +58,8 @@ void	my_mlx_pixel_put_mini(t_cub3d *data, double width \
 	data->mini_img->addr = mlx_get_data_addr(data->mini_img->img_ptr,
 			&data->mini_img->bits_per_pixel, &data->mini_img->line_length,
 			&data->mini_img->endian);
-	dst = data->mini_img->addr + (y * data->mini_img->line_length + \
-	x * (data->mini_img->bits_per_pixel / 8));
+	dst = data->mini_img->addr + (y * data->mini_img->line_length + x
+			* (data->mini_img->bits_per_pixel / 8));
 	if ((x >= 0 && x < MINI_WIDTH) && (y >= 0 && y < MINI_HEIGHT))
 		*(unsigned int *)dst = color;
 }
